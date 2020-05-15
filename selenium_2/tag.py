8a620d2b9b28e31b04cf0936bf42be0ebb46d621
Ting6001 <nangnt1040613@gmail.com>
2020-05-15 14:24:24 +0800

    每標記50筆就先存檔，避免意外發生QQ

--- selenium_2/tag.py
+++ selenium_2/tag.py
@@ -3,6 +3,16 @@ import glob
 import os
 import numpy as np
 
+save_num = 50 # 每標記幾筆就先存檔，避免意外發生QQ
+def save_csv(df):
+	if choose == 't':
+		series_tag = pd.Series(lst_tag).astype(str) # 預設空的Series Type會是float64，要手動指定為str
+		series_dish = pd.Series(lst_dish).astype(str) 
+		df['tag'] = series_tag
+		df['dish'] = series_dish
+	newpath = path + name + '_tag.csv'
+	df.to_csv(newpath, index=False, encoding="utf-8")
+
 def tag(df):
 	curr_count = ''
 	if choose == 't':
@@ -11,12 +21,15 @@ def tag(df):
 			print('\n', curr_count, row['text'])
 			lst_tag.append(input('請標記情緒(a正面，b中性，c負):'))
 			lst_dish.append(input('請標記菜名(菜,情緒，每道菜依?隔開):'))
-		series_dish = pd.Series(lst_dish)
-		series_tag = pd.Series(lst_tag)
-		df['tag'] = series_tag
-		df['dish'] = series_dish
+
+			if (row['id'] % save_num == 0):
+				print('每', save_num, '筆先存檔')
+				save_csv(df)
+		# print(df.dtypes)
 	elif choose == 'c':
 		for index, row in df.iterrows():# id,name,date,star,text,dish,tag
+			df['tag'] = df['tag'].astype(str)  # 預設空的Series Type會是float64，要手動指定為str
+			df['dish'] = df['dish'].astype(str)
 			curr_count = str(row['id']) + '/' + str(len(df.index))
 			show_tag = '【' + str(row['tag']) + '】'
 			show_dish = '【' + str(row['dish']) + '】'
@@ -34,6 +47,9 @@ def tag(df):
 					df.at[index, 'dish'] = np.nan
 				else:
 					df.at[index, 'dish'] = newdish
+			if (row['id'] % save_num == 0):
+				print('每', save_num, '筆先存檔')
+				save_csv(df)
 
 district = input('請輸入你的行政區:')
 choose = input('請選擇要 t標記 or c檢查:')
@@ -52,11 +68,11 @@ for i in range(store_count):
 	file = files[i]
 	name = os.path.basename(file).split('_')[0]
 	df = pd.read_csv(file, encoding='utf8', engine='python')
-
 	lst_dish = []
 	lst_tag = []
 	print('======', str(i+1) + '/' + str(store_count), name, ',評論總共有', len(df.index), '筆 ======', end='  >>>')
 	ans = input('繼續標註? q離開 n換下一家店：')
+
 	if ans  == 'q':
 		break
 	elif ans == 'n':
@@ -64,9 +80,9 @@ for i in range(store_count):
 	if len(df.index) == 0:
 		print('這家店:', name, ',完全沒有評論，換下一家店')
 		continue
+	
+	# print(df.dtypes)
 	tag(df)
-
-	print('============ 這家店標記完成 ============')
+	save_csv(df)
 	print(df)
-	newpath = path + name + '_tag.csv'
-	df.to_csv(newpath, index=False, encoding="utf-8")
+	print('============ 這家店標記完成 ============')
